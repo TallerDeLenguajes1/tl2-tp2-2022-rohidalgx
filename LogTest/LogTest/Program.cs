edd8307a6bb387eb1be2a7dc324427d6af4dbe79
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-09-09 19:04:53 -0300

    ...

---
+++ LogTest/LogTest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+           Logger.Info("Hello world");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
